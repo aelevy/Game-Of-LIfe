1c70508832531f08097a9dc127ab82dae7bea7d2
Andrew Levy <levyandrew44@gmail.com>
2016-05-05 12:57:20 -0400

    grid generation is working

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -3,12 +3,27 @@
 angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
+  $routeProvider.when('/', {
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl'
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope',function($scope) {
+	 $scope.rows = 0;
+	 $scope.columns = 0;
+	 $scope.getRows = function(rows){
+	 	return new Array(rows);
+	 }; 
+	 $scope.getColumns = function(columns){
+	 	return new Array(columns);
+	 }; 
 
+	 // $scope.Neighbors = function(){
+	 // 	 create the concept of the 8 neighbors?
+	 // 	};
+
+ 	//  $scope.GameOfLife = function(Neighbors){
+ 	//  	using Neighbors create an if block around the 4 rules
+ 	//  }
 }]);
